058068aa8360bd8b1278df2cfd4ba42ef2c3c647
Ying Huang <huangying270@gmail.com>
2019-07-01 02:00:50 -0400

    adding if and else and pushing to array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,13 @@
-var computerPokemonName = ["pikachu", "squirtle", "bulbasaur", "charmander", "meowth"];
+var computerPokemonName = ["PIKACHU", "SQUIRTLE", "BULBASAUR", "CHARMANDER", "MEOWTH"];
 
 var computerPokemonNumber = Math.floor(Math.random() * computerPokemonName.length);
 var PokemonChoice = computerPokemonName[computerPokemonNumber];
 console.log(PokemonChoice);
 
+var rightLetter = [];
+var wrongLetter = [];
 var Underscores = [];
+
 var generateUnderscore = ( ) => {
     for (var i = 0; i < PokemonChoice.length; i++) {
         Underscores.push("_");
@@ -14,8 +17,25 @@ var generateUnderscore = ( ) => {
 console.log(generateUnderscore());
 
 document.onkeyup = function(event) {
-    console.log(event);
-    
-    var keycode = event.keyCode;
-    console.log(keycode);
+
+    var keyword = String.fromCharCode(event.keyCode);
+    console.log(PokemonChoice.indexOf(keyword));
+
+    if(keyword.indexOf(PokemonChoice) > -1) {
+
+        rightLetter.push(keyword);
+        console.log(rightLetter);
+        console.log(true);
+
+        Underscores[PokemonChoice.indexOf(keyword)] = keyword;
+        console.log(Underscores);
+
+        if(Underscores.join("") == PokemonChoice) {
+            alert("You Win!");
+        }
+
+    } else {
+        wrongLetter.push(keyword);
+        console.log(wrongLetter);
+    }
 }
